d2d5f6589a6c7a7b4987b671732f2be04932c195
Mombrini <paolamombriniphoto@gmail.com>
2023-05-15 12:19:53 +0200

    modfica

--- Esercizio_3/esempi/3_palloncini/sketch.js
+++ Esercizio_3/esempi/3_palloncini/sketch.js
@@ -3,23 +3,25 @@ let posizioneX
 let velX
 let velY
 
-function setup(){
+function setup()
+{
 	createCanvas(800, 600)
-	posizioneX = width/2
-	posizioneY = height/2 
-	velX = 3
-	velY = 3
-	background(255, 60, 0)
+	posizioneX = width/3
+	posizioneY = height/2
+	velX = 1
+	velY = 1
+	background(255, 200, 20)
 }
 
-function draw(){
+function draw()
+{
 	noStroke()
 	//fill(map(posizioneX, 0, width, 0, 255), map(posizioneY, 0, width, 0, 255), 0)
 
 	const r = (sin(frameCount * 0.031) + 1) * 127.5
-	const g = (sin(frameCount * 0.032) + 1) * 127.5
+	const g = (cos(frameCount * 0.032) + 1) * 127.5
 	const b = (sin(frameCount * 0.033) + 1) * 127.5
-	fill(r, g, 0) 
+	fill(r, 0, b) 
 
 	const d = sin(frameCount * 0.06) * 60 + 70
 
@@ -29,10 +31,14 @@ function draw(){
 	posizioneX = posizioneX + velX
 	posizioneY = posizioneY + velY
 
+	//if (posizioneX >= width) posizioneX = 1 
+	//if (posizioneY >= height) posizioneY = 1 
+
 	if (posizioneX >= width || posizioneX <= 0) velX = -velX
 	if (posizioneY >= height || posizioneY <= 0) velY = -velY
 }
 
-function keyPressed(){
+function keyPressed()
+{
 	if (key == 's') save("pong.png") 
 }
---
+++ Esercizio_3/esempi/5_pioggia/pioggia.js
@@ -0,0 +1,48 @@
+var canvas = document.getElementById('snowCanvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var snowflakes = [];
+
+function Snowflake() {
+  this.x = Math.random() * canvas.width;
+  this.y = -50;
+  this.speed = Math.random() * 6 + 8;
+  this.radius = 1 //Math.random() * 4 + 1;
+  this.opacity = Math.random() * 0.5 + 0.5;
+
+  this.update = function() {
+    this.y += this.speed;
+    if (this.y > canvas.height) {
+      this.y = -50;
+      this.x = Math.random() * canvas.width;
+    }
+  };
+
+  this.draw = function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "rgba(0, 0, 0, " + this.opacity + ")";
+    ctx.fill();
+  };
+}
+
+function createSnowflakes() {
+  for (var i = 0; i < 500; i++) {
+    snowflakes.push(new Snowflake());
+  }
+}
+
+function animateSnowflakes() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < snowflakes.length; i++) {
+    snowflakes[i].update();
+    snowflakes[i].draw();
+  }
+  requestAnimationFrame(animateSnowflakes);
+}
+
+createSnowflakes();
+animateSnowflakes();
